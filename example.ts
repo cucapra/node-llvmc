2f25745944a435e31464d8c97a5decc9e7ca1d8b
Adrian Sampson <adrian@radbox.org>
2017-02-20 16:57:42 -0500

    Build a little IR

--- example.ts
+++ example.ts
@@ -1,10 +1,29 @@
 import { LLVM } from './src/llvmc';
 
+// Create a module.
 let mod = LLVM.LLVMModuleCreateWithName("some_module");
+
+// Add a function to the module.
+let paramtypes = new Buffer(0);
+let functype = LLVM.LLVMFunctionType(LLVM.LLVMInt32Type(), paramtypes, 0, 0);
+let main = LLVM.LLVMAddFunction(mod, "main", functype);
+
+// Add a single basic block to the function.
+let entry = LLVM.LLVMAppendBasicBlock(main, "entry");
+
+// Build a tiny program in the block.
+let builder = LLVM.LLVMCreateBuilder();
+LLVM.LLVMPositionBuilderAtEnd(builder, entry);
+LLVM.LLVMDisposeBuilder(builder);
+
+// Dump the IR as bitcode to disk.
 let err = LLVM.LLVMWriteBitcodeToFile(mod, "out.bc");
 if (err) {
   console.error("write failed", err);
 }
+
+// Write the IR as text to the console.
 let ir = LLVM.LLVMPrintModuleToString(mod);
 console.log(ir);
+
 LLVM.LLVMDisposeModule(mod);
--- src/llvmc.ts
+++ src/llvmc.ts
@@ -8,14 +8,41 @@ let void_ = ref.types.void;
 export const LLVM = ffi.Library('libLLVM', {
   // Modules.
   // http://llvm.org/docs/doxygen/html/group__LLVMCCoreModule.html
-  'LLVMModuleCreateWithName': [voidp, ['string']],
-  'LLVMCloneModule':          [voidp, [voidp]],
-  'LLVMDisposeModule':        [void_, [voidp]],
-  'LLVMGetDataLayout':        ['string', [voidp]],
-  'LLVMSetDataLayout':        [void_, [voidp, 'string']],
-  'LLVMGetTarget':            ['string', [voidp]],
-  'LLVMSetTarget':            [void_, [voidp, 'string']],
-  'LLVMDumpModule':           [void_, [voidp]],
-  'LLVMPrintModuleToString':  ['string', [voidp]],
-  'LLVMWriteBitcodeToFile':   ['int', [voidp, 'string']],
+  'LLVMModuleCreateWithName':   [voidp, ['string']],
+  'LLVMCloneModule':            [voidp, [voidp]],
+  'LLVMDisposeModule':          [void_, [voidp]],
+  'LLVMGetDataLayout':          ['string', [voidp]],
+  'LLVMSetDataLayout':          [void_, [voidp, 'string']],
+  'LLVMGetTarget':              ['string', [voidp]],
+  'LLVMSetTarget':              [void_, [voidp, 'string']],
+  'LLVMDumpModule':             [void_, [voidp]],
+  'LLVMPrintModuleToString':    ['string', [voidp]],
+  'LLVMWriteBitcodeToFile':     ['int', [voidp, 'string']],
+  'LLVMAddFunction':            [voidp, [voidp, 'string', voidp]],
+
+  // Function types.
+  // http://llvm.org/docs/doxygen/html/group__LLVMCCoreTypeFunction.html
+  'LLVMFunctionType':           [voidp, [voidp, voidp, ref.types.uint32,
+                                         'bool']],
+
+  // Basic blocks.
+  // http://llvm.org/docs/doxygen/html/group__LLVMCCoreValueBasicBlock.html
+  'LLVMAppendBasicBlock':       [voidp, [voidp, 'string']],
+
+  // Instruction builder.
+  // http://llvm.org/docs/doxygen/html/group__LLVMCCoreInstructionBuilder.html
+  'LLVMCreateBuilder':          [voidp, []],
+  'LLVMPositionBuilder':        [void_, [voidp, voidp, voidp]],
+  'LLVMPositionBuilderBefore':  [void_, [voidp, voidp]],
+  'LLVMPositionBuilderAtEnd':   [void_, [voidp, voidp]],
+  'LLVMDisposeBuilder':         [void_, [voidp]],
+
+  // Types.
+  // http://llvm.org/docs/doxygen/html/group__LLVMCCoreTypeInt.html
+  'LLVMInt1Type':               [voidp, []],
+  'LLVMInt8Type':               [voidp, []],
+  'LLVMInt16Type':              [voidp, []],
+  'LLVMInt32Type':              [voidp, []],
+  'LLVMInt64Type':              [voidp, []],
+  'LLVMInt128Type':             [voidp, []],
 });
