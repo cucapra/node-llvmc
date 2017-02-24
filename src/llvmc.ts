2ec3b5becb02a6050e26a4e5b45ba92448df4def
Richard Henwood <rbh288@cornell.edu>
2017-02-24 15:50:35 -0500

    Add CoreModule function names
    Still need to add param/return types

--- src/llvmc.ts
+++ src/llvmc.ts
@@ -16,17 +16,31 @@ let void_ = ref.types.void;
 export const LLVM = ffi.Library('libLLVM', {
   // Modules.
   // http://llvm.org/docs/doxygen/html/group__LLVMCCoreModule.html
-  'LLVMModuleCreateWithName':   [voidp, ['string']],
-  'LLVMCloneModule':            [voidp, [voidp]],
-  'LLVMDisposeModule':          [void_, [voidp]],
-  'LLVMGetDataLayout':          ['string', [voidp]],
-  'LLVMSetDataLayout':          [void_, [voidp, 'string']],
-  'LLVMGetTarget':              ['string', [voidp]],
-  'LLVMSetTarget':              [void_, [voidp, 'string']],
-  'LLVMDumpModule':             [void_, [voidp]],
-  'LLVMPrintModuleToString':    ['string', [voidp]],
-  'LLVMWriteBitcodeToFile':     ['int', [voidp, 'string']],
-  'LLVMAddFunction':            [voidp, [voidp, 'string', voidp]],
+  'LLVMModuleCreateWithName':           [voidp, ['string']],
+  'LLVMModuleCreateWithNameInContext':  [],
+  'LLVMCloneModule':                    [voidp, [voidp]],
+  'LLVMDisposeModule':                  [void_, [voidp]],
+  'LLVMGetDataLayout':                  ['string', [voidp]],
+  'LLVMSetDataLayout':                  [void_, [voidp, 'string']],
+  'LLVMGetTarget':                      ['string', [voidp]],
+  'LLVMSetTarget':                      [void_, [voidp, 'string']],
+  'LLVMDumpModule':                     [void_, [voidp]],
+  'LLVMPrintModuleToFile':              [],
+  'LLVMPrintModuleToString':            ['string', [voidp]],
+  'LLVMSetModuleInlineAsm':             [],
+  'LLVMGetModuleContext':               [],
+  'LLVMGetTypeByName':                  [],
+  'LLVMGetNamedMetadataNumOperands':    [],
+  'LLVMGetNamedMetadataOperands':       [],
+  'LLVMAddNamedMetadataOperand':        [],
+  'LLVMAddFunction':                    [voidp, [voidp, 'string', voidp]],
+  'LLVMGetNamedFunction':               [],
+  'LLVMGetFirstFunction':               [],
+  'LLVMGetLastFunction':                [],
+  'LLVMGetNextFunction':                [],
+  'LLVMGetPreviousFunction':            [],
+  'LLVMWriteBitcodeToFile':             ['int', [voidp, 'string']],
+  
 
   // Function types.
   // http://llvm.org/docs/doxygen/html/group__LLVMCCoreTypeFunction.html
