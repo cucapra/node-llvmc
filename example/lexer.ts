f39b30bd8b4e56781bc2b47f5da4afd64416bc0d
Richard Henwood <rbh288@cornell.edu>
2017-03-20 00:53:54 -0400

    Split calculator into multiple files

---
+++ example/lexer.ts
@@ -0,0 +1,134 @@
+///////////////////////////////////////
+// Setup
+///////////////////////////////////////
+let prog: string; // program to run
+let lastChar: string = ' ';
+let idx: number = 0; // where we are in program
+let EOF: string = ''; 
+
+export function reset(_prog: string) {
+	prog = _prog;
+	lastChar = ' ';
+	idx = 0;
+}
+
+///////////////////////////////////////
+// Tokens
+///////////////////////////////////////
+export interface Token {
+	id : string;
+};
+
+/// Token representing end of file
+export class Tok_Eof implements Token {
+	public id: string = 'Tok_Eof';
+};
+
+/// Token representing variable declaration 
+export class Tok_Def implements Token {
+	public id: string = 'Tok_Def';
+};
+
+/// Token representing extern
+export class Tok_Ext implements Token {
+	public id: string = 'Tok_Ext'
+}
+
+/// Token representing number
+export class Tok_Number implements Token {
+	public id: string = 'Tok_Number';
+};
+
+/// Token representing variable identifier
+export class Tok_Id implements Token {
+	public id: string = 'Tok_Id';
+};
+
+/// Token representing other things (e.g. parens and operators)
+export class Tok_Other implements Token {
+	public id: string;
+
+	public constructor(id: string) {this.id = id;}
+};
+
+///////////////////////////////////////
+// Lexer
+///////////////////////////////////////
+let numVal: number; // filled with last number seen
+let idStr: string;  // filled with last variable id seen
+
+export function getNumVal() : number{return numVal;}
+export function setNumVal(val: number) : void {numVal = val;}
+export function getIdStr() : string {return idStr;}
+export function setIdStr(id: string) : void {idStr = id;}
+
+/// Read and return next char of program (return EOF if at end of program)
+function getChar() : string {
+	if (idx < prog.length) {
+		let result: string = prog.charAt(idx);
+		idx += 1;
+		return result;
+	}
+	return EOF;
+};
+
+/// Check if str is a single alphabetic character
+function isAlpha(str : string) : boolean {
+  return str.length === 1 && str.match(/[a-z]/i) !== null;
+};
+
+/// Check if str is a single alphanumeric character
+function isAlNum(str : string) : boolean {
+	return str.length === 1 && str.match(/[a-z0-9]/i) !== null;
+};
+
+/// Check if str is one character and a digit
+function isDigit(str : string) : boolean {
+	return str.length === 1 && str.match(/[0-9]/i) !== null;
+};
+
+
+/// Retrive the next token
+export function getTok() : Token {
+	// skip whitespace
+	while (lastChar === ' ') 
+		lastChar = getChar();
+	
+	// identifier: [a-zA-Z][a-zA-Z0-9]*
+	if (isAlpha(lastChar)) { 
+	  idStr = lastChar;
+	  while (isAlNum((lastChar = getChar())))
+	    idStr += lastChar;
+
+	  if (idStr === "def")
+	    return new Tok_Def();
+		if (idStr === "extern")
+			return new Tok_Ext();
+	  return new Tok_Id();
+	} 
+
+	// number: [0-9.]+
+	let seenDecimal: boolean = false;
+	if (isDigit(lastChar) || lastChar === '.') {
+  		let numStr : string = '';
+  		do {
+  			seenDecimal = (lastChar === '.') ? true : seenDecimal;
+    		numStr += lastChar;
+    		lastChar = getChar();
+    		if (lastChar === '.' && seenDecimal) {throw 'Too many decimal points';}
+  		} while (isDigit(lastChar) || lastChar === '.');
+
+  		// we have seen a number token, so set numVal
+  		numVal = parseFloat(numStr);
+  		return new Tok_Number();
+	}
+	
+	// check for end of file
+	if (lastChar === EOF)
+		return new Tok_Eof();
+
+	// Otherwise, just return the character as its ascii value
+	let thisChar : string = lastChar;
+	lastChar = getChar();
+	return new Tok_Other(thisChar);
+};
---
+++ example/parser.ts
@@ -0,0 +1,218 @@
+import * as llvmc from '../src/wrapped';
+import * as readline from 'readline';
+import * as ast from './ast';
+import * as lexer from './lexer';
+
+////////////////////////////////////////////////
+// Parser
+////////////////////////////////////////////////
+
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+let curTok: lexer.Token;
+export function getNextToken() : lexer.Token {
+	curTok = lexer.getTok();
+  	return curTok;
+};
+
+export function getCurTokID() : string {
+  return curTok.id;
+}
+
+/// numberexpr ::= number
+export function parseNumberExpr(): ast.ExprAST|null {
+  let result: ast.ExprAST = new ast.NumberExprAST(lexer.getNumVal());
+  getNextToken(); // consume the number
+  return result;
+};
+
+/// parenexpr ::= '(' expression ')'
+export function parseParenExpr() : ast.ExprAST|null {
+	getNextToken(); // eat (.
+	  
+	let v: ast.ExprAST|null = parseExpression();
+	if (!v)
+	  	return null;
+
+	if (curTok.id !== ')')
+	   	throw 'mismatch parens';
+
+	getNextToken(); // eat ).
+	return v;
+};
+
+/// identifierexpr
+///   ::= identifier
+export function parseIdentifierExpr() : ast.ExprAST|null {
+	let idName: string = lexer.getIdStr();
+
+  getNextToken();  // eat identifier.
+  
+  if (curTok.id !== '(') // Simple variable ref.
+    return new ast.VariableExprAST(idName);
+
+  // Call.
+  getNextToken();  // eat (
+  let args: ast.ExprAST[] = [];
+
+  if (curTok.id !== '(') {
+    while (true) {
+    	let arg: ast.ExprAST|null = parseExpression();
+      
+      if (arg)
+        args.push(arg);
+      else
+        return null;
+
+      if (curTok.id == ')')
+        break;
+
+      if (curTok.id != ',')
+        throw "Expected ')' or ',' in argument list";
+      
+      getNextToken();
+    }
+  }
+
+  // Eat the ')'.
+  getNextToken();
+  return new ast.CallExprAST(idName, args);
+};
+
+/// primary
+///   ::= numberexpr
+///   ::= parenexpr
+export function parsePrimary() : ast.ExprAST|null {
+  	switch (curTok.id) {
+  		case 'Tok_Id':
+    		return parseIdentifierExpr();
+  		case 'Tok_Number':
+   			return parseNumberExpr();
+  		case '(':
+    		return parseParenExpr();
+    	default:
+    		throw 'unknown token';
+  	}
+};
+
+/// BinopPrecedence - This holds the precedence for each binary operator that is
+/// defined.
+let BinopPrecedence:{ [op:string] : number } = {
+	'+': 20,
+	'-': 20,
+	'*': 40,
+};
+
+/// GetTokPrecedence - Get the precedence of the pending binary operator token.
+function getTokPrecedence() : number {
+	if (curTok instanceof lexer.Tok_Eof)
+		return -1;
+
+  // Make sure it's a declared binop.
+  if (BinopPrecedence.hasOwnProperty(curTok.id))
+  	return Number(BinopPrecedence[curTok.id]);
+  return -1;
+}
+
+/// binoprhs
+///   ::= ('+' primary)*
+export function parseBinOpRHS (exprPrec: number, left: ast.ExprAST) : ast.ExprAST|null  {
+  // If this is a binop, find its precedence.
+  while (true) {
+    let tokPrec: number = getTokPrecedence();
+
+    // If this is a binop that binds at least as tightly as the current binop,
+    // consume it, otherwise we are done.
+    if (tokPrec < exprPrec)
+      return left;
+
+    // Okay, we know this is a binop.
+    let binOp: string = curTok.id;
+    getNextToken(); // eat binop
+
+    // Parse the primary expression after the binary operator.
+    let right: ast.ExprAST|null = parsePrimary();
+    if (!right)
+      return null;
+
+    // If BinOp binds less tightly with RHS than the operator after RHS, let
+    // the pending operator take RHS as its LHS.
+    let nextPrec: number = getTokPrecedence();
+    if (tokPrec < nextPrec) {
+      right = parseBinOpRHS(tokPrec + 1, right);
+      if (!right)
+        return null;
+    }
+
+    // Merge LHS/RHS.
+    left = new ast.BinaryExprAST(binOp, left, right);
+  }
+}
+
+/// expression
+///   ::= primary binoprhs
+///
+export function parseExpression() : ast.ExprAST|null {
+	let left : ast.ExprAST|null = parsePrimary();
+  if (!left)
+		return null;
+
+  return parseBinOpRHS(0, left);
+};
+
+/// prototype
+///   ::= id '(' id* ')'
+export function parsePrototype() : ast.PrototypeAST|null {
+  if (!(curTok instanceof lexer.Tok_Id))
+    throw "Expected function name in prototype";
+
+  let fnName: string = lexer.getIdStr();
+  getNextToken();
+
+  if (curTok.id !== '(')
+    throw "Expected '(' in prototype";
+
+  // Read the list of argument names.
+ 	let argNames: string[] = [];
+  while (getNextToken() instanceof lexer.Tok_Id)
+    argNames.push(lexer.getIdStr());
+  
+  if (curTok.id !== ')')
+    throw "Expected ')' in prototype";
+
+  // success.
+  getNextToken();  // eat ')'.
+
+  return new ast.PrototypeAST(fnName, argNames);
+};
+
+/// definition ::= 'def' prototype expression
+export function parseDefinition() : ast.FunctionAST|null {
+  getNextToken();  // eat def.
+  let proto: ast.PrototypeAST|null = parsePrototype();
+  if (!proto) 
+    return null;
+
+  let exp: ast.ExprAST|null = parseExpression();
+  if (exp)
+    return new ast.FunctionAST(proto, exp);
+  return null;
+};
+
+/// external ::= 'extern' prototype
+export function parseExtern() : ast.PrototypeAST|null {
+  getNextToken();  // eat extern.
+  return parsePrototype();
+};
+
+/// toplevelexpr ::= expression
+export function parseTopLevelExpr() : ast.FunctionAST|null {
+	let exp: ast.ExprAST|null = parseExpression();
+  if (exp) {
+    // Make an anonymous proto.
+    let proto: ast.PrototypeAST = new ast.PrototypeAST("", []);
+    return new ast.FunctionAST(proto, exp);
+  }
+  return null;
+}
